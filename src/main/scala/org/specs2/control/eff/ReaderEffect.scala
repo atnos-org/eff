28e1b04cccb5f81c039462e88ef62c9752dca1f7
Eric Torreborre <etorreborre@yahoo.com>
2016-01-08 10:37:32 +1100

    add local for Reader

--- src/main/scala/org/specs2/control/eff/ReaderEffect.scala
+++ src/main/scala/org/specs2/control/eff/ReaderEffect.scala
@@ -19,13 +19,17 @@ import Tag._
 object ReaderEffect {
 
   /** get the environment */
-  def ask[R, I](implicit member: Member[Reader[I, ?], R]): Eff[R, I] =
-    send[Reader[I, ?], R, I](Reader(identity _))
+  def ask[R, T](implicit member: Member[Reader[T, ?], R]): Eff[R, T] =
+    local[R, T, T](identity)
+
+  /** modify the environment */
+  def local[R, T, U](f: T => U)(implicit member: Member[Reader[T, ?], R]): Eff[R, U] =
+    send[Reader[T, ?], R, U](Reader(f))
 
   /** interpret the Reader effect by providing an environment when required */
   def runReader[R <: Effects, A, B](env: A)(r: Eff[Reader[A, ?] |: R, B]): Eff[R, B] = {
     val recurse = new Recurse[Reader[A, ?], R, B] {
-      def apply[X](m: Reader[A, X]) = left(env.asInstanceOf[X])
+      def apply[X](m: Reader[A, X]) = left(m.run(env))
     }
 
     interpret1[R, Reader[A, ?], B, B]((b: B) => b)(recurse)(r)
@@ -34,7 +38,7 @@ object ReaderEffect {
   /** interpret a tagged Reader effect by providing an environment when required */
   def runTaggedReader[R <: Effects, T, A, B](env: A)(r: Eff[({type l[X] = Reader[A, X] @@ T})#l |: R, B]): Eff[R, B] = {
     val recurse = new Recurse[({type l[X] = Reader[A, X] @@ T})#l, R, B] {
-      def apply[X](m: Reader[A, X] @@ T) = Left(env.asInstanceOf[X])
+      def apply[X](m: Reader[A, X] @@ T) = Left(Tag.unwrap(m).run(env))
     }
 
     interpret1[R, ({type l[X] = Reader[A, X] @@ T})#l, B, B]((b: B) => b)(recurse)(r)
---
+++ src/test/scala/org/specs2/control/eff/ReaderEffectSpec.scala
@@ -0,0 +1,31 @@
+package org.specs2.control.eff
+
+import org.specs2.Specification
+import Eff._, Effects._, ReaderEffect._
+import cats.data._
+import cats.syntax.all._
+
+class ReaderEffectSpec extends Specification { def is = s2"""
+
+ local can be used to "zoom" on a configuration $localEffect
+
+"""
+
+  def localEffect = {
+    type R[A] = Reader[Config, A]
+    type S = R |: NoEffect
+
+    implicit def rm: Member[R, S] =
+      Member.MemberNatIsMember
+
+    val action: Eff[S, (Int, String)] = for {
+      f <- local[S, Config, Int]((_:Config).factor)
+      h <- local[S, Config, String]((_:Config).host)
+    } yield (f, h)
+
+    run(runReader(Config(10, "www.me.com"))(action)) ==== ((10, "www.me.com"))
+  }
+
+  case class Config(factor: Int, host: String)
+
+}
