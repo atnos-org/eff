6b673872e69fd1141eb59f3bddedf3e2f8244972
Eric Torreborre <etorreborre@yahoo.com>
2016-04-25 18:23:10 +0200

    added some documentation on `transform` and `localReader`

--- src/test/scala/org/atnos/site/TransformStack.scala
+++ src/test/scala/org/atnos/site/TransformStack.scala
@@ -9,8 +9,59 @@ object TransformStack extends UserGuidePage { def is = "Transforming stacks".tit
 
 ### Transform an effect to another
 
+#### Change the effect
+
+A typical use case for this is to transform a stack having a `Reader[S, ?]` effect to a stack having a `Reader[B, ?]` effect
+ where `S` is "contained" in `B` (meaning that there is a mapping from `B`, "big", to `S`, "small"). Here is an example:${snippet{
+import org.atnos.eff._, all._
+import org.atnos.eff.syntax.all._
+import cats.data._
+
+case class Conf(host: String, port: Int)
+
+type ReaderPort[A] = Reader[Int, A]
+type ReaderHost[A] = Reader[String, A]
+type ReaderConf[A] = Reader[Conf, A]
+
+type S1 = ReaderHost |: Option |: NoEffect
+type S2 = ReaderPort |: Option |: NoEffect
+type SS = ReaderConf |: Option |: NoEffect
+
+implicit val ReaderHostMember: Member.Aux[ReaderHost, S1, Option |: NoEffect] =
+  Member.first
+
+implicit val ReaderPortMember: Member.Aux[ReaderPort, S2, Option |: NoEffect] =
+  Member.first
+
+implicit val ReaderConfMember: Member.Aux[ReaderConf, SS, Option |: NoEffect] =
+  Member.first
+
+val readHost: Eff[S1, String] = for {
+  c <- ask[S1, String]
+  h <- OptionEffect.some[S1, String]("hello")
+} yield h
+
+val readPort: Eff[S2, String] = for {
+  c <- ask[S2, Int]
+  h <- OptionEffect.some[S2, String]("world")
+} yield h
+
+val action: Eff[SS, String] = for {
+  s1 <- localReader(readHost, (c: Conf) => c.host)
+  s2 <- localReader(readPort, (c: Conf) => c.port)
+} yield s1 + " " + s2
+
+action.runReader(Conf("www.me.com", 8080)).runOption.run
+
+}.eval}
+
+More generally the method `Interpret.transform(eff, naturalTransformation)` provides a way to interpret one effect to
+another using a natural transformation (changing just one effect in the stack).
+
+#### From one effect to another in the same stack
+
 Once you get a `Eff[R, A]` action you might want to act on one of the effects, for example to swap the `Option` effect
-with the `Either` effect:${snippet{
+with the `Either` effect if both are present in the stack:${snippet{
 import org.atnos.eff._, all._
 import org.atnos.eff.syntax.all._
 import cats.data._
@@ -38,6 +89,7 @@ def addKeys(key1: String, key2: String): Eff[S, Int] = for {
   b <- option(map.get(key2))
 } yield a + b
 
+// describe how options are transformed into eithers
 def nat(message: String) = new NaturalTransformation[Option, XorString] {
   def apply[A](o: Option[A]) = o.fold(Xor.left[String, A](message))(Xor.right[String, A])
 }
@@ -52,7 +104,9 @@ import org.atnos.eff.implicits._
  addKeysWithDefaultMessage("key1", "missing", "Key not found").runOption.runXor.run)
 
 }.eval}
-                                                                                        
+
+Note that the stack stays the same before and after, even if the original effect is not used anymore.
+
 ### Merge stacks
 
 We have seen, in the ${"open-closed" ~/ OpenClosed} section, that we can create effects for a given effect stack, for example
