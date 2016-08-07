labra/eff-cats

added a new representation for effects

jvm/src/test/scala/org/atnos/example/Action.scala
@@ -35,21 +35,24 @@ import Member.<=
  * Also note that Eval is the last effect which means that nothing get evaluated until we run the last interpreter
  *
  */
-object Action extends ActionCreation with ActionImplicits
+object Action extends ActionCreation with ActionInterpretation
 
 trait ActionTypes {
-  type ActionStack = ErrorOrOk |: Console |: Warnings |: Eval |: NoEffect
+  type ActionStack_ = ErrorOrOk |: Console |: Warnings |: Eval |: NoEffect
+  val as = Fx[ActionStack_]
+  type ActionStack = as.Fx
 }
 
 trait ActionCreation extends ActionTypes {
   /**
-   * warn the user about something that is probably wrong on his side,
-   * and then fail all other computations
-   */
-  def warnAndFail[R, A](message: String, failureMessage: String)(implicit m1: Warnings <= R, m2: ErrorOrOk <= R): Eff[R, A] =
+    * warn the user about something that is probably wrong on his side,
+    * and then fail all other computations
+    */
+  def warnAndFail[R, A](message: String, failureMessage: String)(implicit m1: Warnings |= R, m2: ErrorOrOk |= R): Eff[R, A] =
     warn(message)(m1) >>
       fail(failureMessage)
-
+}
+trait ActionInterpretation extends ActionImplicits {
   def runAction[A](action: Eff[ActionStack, A], printer: String => Unit = s => ()): (Error Xor A, List[String]) =
     run(runEval(runWarnings(runConsoleToPrinter(printer)(runError(action)))))
 }
@@ -58,17 +61,17 @@ object ActionCreation extends ActionCreation
 
 trait ActionImplicits extends ActionTypes {
 
-  implicit def ErrorOrOkMember =
-    implicitly[Member.Aux[ErrorOrOk, ActionStack, Console |: Warnings |: Eval |: NoEffect]]
+  implicit def ErrorOrOkMember: Member.Aux[ErrorOrOk, ActionStack, In3[Console, Warnings, Eval]] =
+    Member.Member4L[ErrorOrOk, Console, Warnings, Eval]
 
   implicit def ConsoleMember =
-    implicitly[Member.Aux[Console, ActionStack, ErrorOrOk |: Warnings |: Eval |: NoEffect]]
+    Member.MemberAppendR[Console, In1[ErrorOrOk], In3[Console, Warnings, Eval], In2[Warnings, Eval]](Member.Member3L[Console, Warnings, Eval])
 
   implicit def WarningsMember =
-    implicitly[Member.Aux[Warnings, ActionStack, ErrorOrOk |: Console |: Eval |: NoEffect]]
+    Member.MemberAppendR[Warnings, In1[ErrorOrOk], In3[Console, Warnings, Eval], In2[Console, Eval]](Member.Member3M[Console, Warnings, Eval])
 
   implicit def EvalMember =
-    implicitly[Member.Aux[Eval, ActionStack, ErrorOrOk |: Console |: Warnings |: NoEffect]]
+    Member.MemberAppendR[Eval, In1[ErrorOrOk], In3[Console, Warnings, Eval], In2[Console, Warnings]](Member.Member3R[Console, Warnings, Eval])
 }
 
 object ActionImplicits extends ActionImplicits

atnos-org/eff

jvm/src/test/scala/org/atnos/example/Action.scala
@@ -35,21 +35,24 @@ import Member.<=
  * Also note that Eval is the last effect which means that nothing get evaluated until we run the last interpreter
  *
  */
-object Action extends ActionCreation with ActionImplicits
+object Action extends ActionCreation with ActionInterpretation
 
 trait ActionTypes {
-  type ActionStack = ErrorOrOk |: Console |: Warnings |: Eval |: NoEffect
+  type ActionStack_ = ErrorOrOk |: Console |: Warnings |: Eval |: NoEffect
+  val as = Fx[ActionStack_]
+  type ActionStack = as.Fx
 }
 
 trait ActionCreation extends ActionTypes {
   /**
-   * warn the user about something that is probably wrong on his side,
-   * and then fail all other computations
-   */
-  def warnAndFail[R, A](message: String, failureMessage: String)(implicit m1: Warnings <= R, m2: ErrorOrOk <= R): Eff[R, A] =
+    * warn the user about something that is probably wrong on his side,
+    * and then fail all other computations
+    */
+  def warnAndFail[R, A](message: String, failureMessage: String)(implicit m1: Warnings |= R, m2: ErrorOrOk |= R): Eff[R, A] =
     warn(message)(m1) >>
       fail(failureMessage)
-
+}
+trait ActionInterpretation extends ActionImplicits {
   def runAction[A](action: Eff[ActionStack, A], printer: String => Unit = s => ()): (Error Xor A, List[String]) =
     run(runEval(runWarnings(runConsoleToPrinter(printer)(runError(action)))))
 }
@@ -58,17 +61,17 @@ object ActionCreation extends ActionCreation
 
 trait ActionImplicits extends ActionTypes {
 
-  implicit def ErrorOrOkMember =
-    implicitly[Member.Aux[ErrorOrOk, ActionStack, Console |: Warnings |: Eval |: NoEffect]]
+  implicit def ErrorOrOkMember: Member.Aux[ErrorOrOk, ActionStack, Fx3[Console, Warnings, Eval]] =
+    Member.Member4L[ErrorOrOk, Console, Warnings, Eval]
 
   implicit def ConsoleMember =
-    implicitly[Member.Aux[Console, ActionStack, ErrorOrOk |: Warnings |: Eval |: NoEffect]]
+    Member.MemberAppendR[Console, Fx1[ErrorOrOk], Fx3[Console, Warnings, Eval], Fx2[Warnings, Eval]](Member.Member3L[Console, Warnings, Eval])
 
   implicit def WarningsMember =
-    implicitly[Member.Aux[Warnings, ActionStack, ErrorOrOk |: Console |: Eval |: NoEffect]]
+    Member.MemberAppendR[Warnings, Fx1[ErrorOrOk], Fx3[Console, Warnings, Eval], Fx2[Console, Eval]](Member.Member3M[Console, Warnings, Eval])
 
   implicit def EvalMember =
-    implicitly[Member.Aux[Eval, ActionStack, ErrorOrOk |: Console |: Warnings |: NoEffect]]
+    Member.MemberAppendR[Eval, Fx1[ErrorOrOk], Fx3[Console, Warnings, Eval], Fx2[Console, Warnings]](Member.Member3R[Console, Warnings, Eval])
 }
 
 object ActionImplicits extends ActionImplicits