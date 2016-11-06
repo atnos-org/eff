16529a1827b50993081542ad452e8adb2e788e50
Edmund Noble <edmundnoble@gmail.com>
2016-11-06 17:45:37 -0500

    Add >> and << to Eff so they can be used applicatively

--- shared/src/main/scala/org/atnos/eff/Eff.scala
+++ shared/src/main/scala/org/atnos/eff/Eff.scala
@@ -65,6 +65,12 @@ sealed trait Eff[R, A] {
   def <*[B](fb: Eff[R, B]): Eff[R, A] =
     EffApplicative.product(this, fb).map { case (a, b) => a }
 
+  def >>[B](fb: Eff[R, B]): Eff[R, B] =
+    EffApplicative.product(this, fb).map { case (a, b) => b }
+
+  def <<[B](fb: Eff[R, B]): Eff[R, A] =
+    EffApplicative.product(this, fb).map { case (a, b) => a }
+
   def flatMap[B](f: A => Eff[R, B]): Eff[R, B] =
     EffMonad[R].flatMap(this)(f)
 
