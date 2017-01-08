a9dcb42e46335aa27f6d6456c7c946a0516e15b6
Eric Torreborre <etorreborre@users.noreply.github.com>
2017-01-08 22:11:02 +0100

    Updated the try-eff script (#51)
    * updated the try-eff script
    
    * added more retries for flaky tests

--- try-eff.sh
+++ try-eff.sh
@@ -2,7 +2,7 @@
 test -e ~/.coursier/cr || (mkdir -p ~/.coursier && wget -q -O ~/.coursier/cr https://git.io/vgvpD && chmod +x ~/.coursier/cr)
 CLASSPATH="$(~/.coursier/cr fetch -q -p \
   \
-  org.atnos:eff_2.11:2.0.0 \
-  com.lihaoyi:ammonite-repl_2.11.7:0.5.2 \
+  org.atnos:eff_2.11:2.3.0 \
+  com.lihaoyi:ammonite_2.11.8:0.8.1 \
   \
-)" java ammonite.repl.Main --predef 'import org.atnos.eff._, all._, syntax.all._'
+)" java ammonite.Main --predef 'import org.atnos.eff._, all._, syntax.all._'
