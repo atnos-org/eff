69b4daab0319c4b883df46f6f815c327d842a8e3
Edmund Noble <Edmund Noble>
2017-01-11 20:25:50 -0500

    Basic fs2 task

--- fs2/jvm/src/main/scala/org/atnos/eff/addon/fs2/package.scala
+++ fs2/jvm/src/main/scala/org/atnos/eff/addon/fs2/package.scala
@@ -3,7 +3,7 @@ package org.atnos.eff.addon
 import org.atnos.eff.ExecutorServices
 import _root_.fs2.{Scheduler, Strategy}
 
-package object fs2 extends AsyncTaskInterpreter {
+package object fs2 extends AsyncTaskInterpreter with TaskEffect {
   /** implement in js and jvm */
   def fromExecutorServices(es: ExecutorServices): AsyncTasks = {
     val s: Strategy = Strategy.fromExecutionContext(es.executionContext)
