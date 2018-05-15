a9c64114ad947eec5d350b476a785db4e213ccfb
Ben Hutchison <brhutchison@gmail.com>
2018-05-15 16:50:25 +1000

    Update CommunityResources.scala (#144)

--- src/test/scala/org/atnos/site/CommunityResources.scala
+++ src/test/scala/org/atnos/site/CommunityResources.scala
@@ -6,6 +6,10 @@ A collection of resources related to Eff.
 
 ### Blog posts
 - [A Journey into Extensible Effects in Scala](http://rea.tech/a-journey-into-extensible-effects-in-scala/)
+
+### Tutorials & Examples
+- [Getting work done with Effectful Functional Programming](https://github.com/benhutchison/GettingWorkDoneWithExtensibleEffects)
+
 """
 
 }
