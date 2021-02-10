3e44a5e5c9d24f93d11f1e3438dab3eb0d4ec51b
xuwei-k <6b656e6a69@gmail.com>
2021-02-10 12:51:49 +0900

    Update ammonite

--- try-eff.sh
+++ try-eff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 test -e ~/.coursier/cr || (mkdir -p ~/.coursier && wget -q -O ~/.coursier/cr https://git.io/vgvpD && chmod +x ~/.coursier/cr)
 ~/.coursier/cr launch -q -P -M ammonite.Main \
-  com.lihaoyi:ammonite_2.13.2:2.1.1 \
+  com.lihaoyi:ammonite_2.13.3:2.3.8 \
   org.atnos:eff_2.13:5.14.0 \
   -- --predef-code 'import org.atnos.eff._, all._, syntax.all._' < /dev/tty
