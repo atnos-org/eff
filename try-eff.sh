047743d7c3ccec7201fe3cb3324961919b36192e
kenji yoshida <6b656e6a69@gmail.com>
2021-05-23 08:53:19 +0900

    Update try-eff.sh

--- try-eff.sh
+++ try-eff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-test -e ~/.coursier/cr || (mkdir -p ~/.coursier && wget -q -O ~/.coursier/cr https://git.io/vgvpD && chmod +x ~/.coursier/cr)
-~/.coursier/cr launch -q -P -M ammonite.Main \
+test -e ~/.coursier/cs || (mkdir -p ~/.coursier && wget -q -O ~/.coursier/cs https://git.io/coursier-cli-"$(uname | tr LD ld)" && chmod +x ~/.coursier/cs)
+~/.coursier/cs launch -q -P -M ammonite.Main \
   com.lihaoyi:ammonite_2.13.3:2.3.8 \
   org.atnos:eff_2.13:5.16.0 \
   -- --predef-code 'import org.atnos.eff._, all._, syntax.all._' < /dev/tty
