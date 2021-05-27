6503b9e609955cf577ec6665c2ab108de9dd24c6
xuwei-k <6b656e6a69@gmail.com>
2021-05-27 23:15:06 +0900

    separate Cache.scala

---
+++ shared/src/main/scala-2/org/atnos/eff/ConcurrentWeakIdentityHashMap.scala
@@ -0,0 +1,27 @@
+package org.atnos.eff
+
+import cats.Eval
+
+case class ConcurrentWeakIdentityHashMapCache(
+  map: ConcurrentWeakIdentityHashMap[AnyRef, Eval[Any]] = new ConcurrentWeakIdentityHashMap[AnyRef, Eval[Any]]) extends Cache {
+
+  type C = Cache
+
+  def memo[V](key: AnyRef, value: =>V): V = {
+    lazy val v = value
+    if (map.putIfAbsent(key.asInstanceOf[AnyRef], Eval.later(v).memoize) == null) v
+    else map.get(key.asInstanceOf[AnyRef]).value.asInstanceOf[V]
+  }
+
+  def put[V](key: AnyRef, value: V): V =
+    map.put(key, Eval.now(value)).value.asInstanceOf[V]
+
+  def get[V](key: AnyRef): Option[V] =
+    Option(map.get(key)).map(_.value.asInstanceOf[V])
+
+  def reset(key: AnyRef) = {
+    map.remove(key.hashCode)
+    this
+  }
+
+}
