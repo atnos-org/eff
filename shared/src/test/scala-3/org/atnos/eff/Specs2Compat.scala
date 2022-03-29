ec24645cacbdf0f39b5f1126ed836cf2dbc13a65
xuwei-k <6b656e6a69@gmail.com>
2022-03-30 08:51:10 +0900

    avoid specs2 5.0.0-ALPHA

--- shared/src/test/scala-3/org/atnos/eff/Specs2Compat.scala
+++ shared/src/test/scala-3/org/atnos/eff/Specs2Compat.scala
@@ -1,12 +1,3 @@
 package org.atnos.eff
 
-import org.specs2.Specification
-
-trait Specs2Compat { self: Specification =>
-
-  implicit final class MustEqualExtension[A](a1: A) {
-    def ====(a2: A) = a1 must beEqualTo(a2)
-    def must_==(a2: A) = a1 must beEqualTo(a2)
-  }
-
-}
+trait Specs2Compat
