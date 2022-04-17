4c3ad92ae46a8ce80481efa544f21adcbd0b7238
xuwei-k <6b656e6a69@gmail.com>
2022-04-17 11:06:41 +0900

    update ammonite

--- try-eff.sh
+++ try-eff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 test -e ~/.coursier/cs || (mkdir -p ~/.coursier && wget -q -O ~/.coursier/cs https://git.io/coursier-cli-"$(uname | tr LD ld)" && chmod +x ~/.coursier/cs)
 ~/.coursier/cs launch -q -P -M ammonite.Main \
-  com.lihaoyi:ammonite_2.13.3:2.3.8 \
-  org.atnos:eff_2.13:6.0.0 \
+  com.lihaoyi:ammonite_3.1.2:2.5.3 \
+  org.atnos:eff_3:6.0.0 \
   -- --predef-code 'import org.atnos.eff._, all._, syntax.all._' < /dev/tty
