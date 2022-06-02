03a3677edb53e52920d1a1dcdd19c6ac8e31b803
xuwei-k <6b656e6a69@gmail.com>
2022-06-03 07:40:04 +0900

    6.0.1

--- try-eff.sh
+++ try-eff.sh
@@ -2,5 +2,5 @@
 test -e ~/.coursier/cs || (mkdir -p ~/.coursier && wget -q -O ~/.coursier/cs https://git.io/coursier-cli-"$(uname | tr LD ld)" && chmod +x ~/.coursier/cs)
 ~/.coursier/cs launch -q -P -M ammonite.Main \
   com.lihaoyi:ammonite_3.1.2:2.5.3 \
-  org.atnos:eff_3:6.0.0 \
+  org.atnos:eff_3:6.0.1 \
   -- --predef-code 'import org.atnos.eff._, all._, syntax.all._' < /dev/tty
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-ThisBuild / version := "6.1.0-SNAPSHOT"
+ThisBuild / version := "6.0.1"
