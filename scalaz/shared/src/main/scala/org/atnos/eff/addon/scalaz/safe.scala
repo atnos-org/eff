db8dd79c066111d626ce4fc68673893989ad0a64
xuwei-k <6b656e6a69@gmail.com>
2023-08-07 16:47:39 +0900

    avoid org.atnos.eff.all

--- scalaz/shared/src/main/scala/org/atnos/eff/addon/scalaz/safe.scala
+++ scalaz/shared/src/main/scala/org/atnos/eff/addon/scalaz/safe.scala
@@ -9,15 +9,15 @@ object safe extends safe
 trait safe {
 
   def runSafeDisjunction[R, U, A](r: Eff[R, A])(implicit m: Member.Aux[Safe, R, U]): Eff[U, (Throwable \/ A, List[Throwable])] =
-    org.atnos.eff.all.runSafe(r).map(_.leftMap(_.fold(\/.left, \/.right)))
+    org.atnos.eff.SafeEffect.runSafe(r).map(_.leftMap(_.fold(\/.left, \/.right)))
 
   def execSafeDisjunction[R, U, A](r: Eff[R, A])(implicit m: Member.Aux[Safe, R, U]): Eff[U, Throwable \/ A] =
-    org.atnos.eff.all.execSafe(r).map(_.fold(\/.left, \/.right))
+    org.atnos.eff.SafeEffect.execSafe(r).map(_.fold(\/.left, \/.right))
 
   def attemptSafeDisjunction[R, A](r: Eff[R, A])(implicit m: Safe /= R): Eff[R, (Throwable \/ A, List[Throwable])] =
-    org.atnos.eff.all.attemptSafe(r).map(_.leftMap(_.fold(\/.left, \/.right)))
+    org.atnos.eff.SafeEffect.attemptSafe(r).map(_.leftMap(_.fold(\/.left, \/.right)))
 
   def attemptDisjunction[R, A](action: Eff[R, A])(implicit m: Safe /= R): Eff[R, Throwable \/ A] =
-    org.atnos.eff.all.attempt(action).map(_.fold(\/.left, \/.right))
+    org.atnos.eff.SafeEffect.attempt(action).map(_.fold(\/.left, \/.right))
 
 }
