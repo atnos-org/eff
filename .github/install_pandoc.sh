d74d003a31a52aa075f0841c82663b81bff6cf48
kenji yoshida <6b656e6a69@gmail.com>
2023-08-13 08:14:56 +0900

    pandoc 3.1.6.1

--- .github/install_pandoc.sh
+++ .github/install_pandoc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-wget https://github.com/jgm/pandoc/releases/download/3.1.5/pandoc-3.1.5-1-amd64.deb
-sudo dpkg -i pandoc-3.1.5-1-amd64.deb
+wget https://github.com/jgm/pandoc/releases/download/3.1.6.1/pandoc-3.1.6.1-1-amd64.deb
+sudo dpkg -i pandoc-3.1.6.1-1-amd64.deb
 pandoc --version
