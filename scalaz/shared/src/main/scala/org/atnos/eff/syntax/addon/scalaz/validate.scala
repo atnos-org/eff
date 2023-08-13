4fa4be00222e5e67df699eb039ed3a04e03c7698
xuwei-k <6b656e6a69@gmail.com>
2023-08-13 15:36:01 +0900

    fix scalaz validate syntax

--- scalaz/shared/src/main/scala/org/atnos/eff/syntax/addon/scalaz/validate.scala
+++ scalaz/shared/src/main/scala/org/atnos/eff/syntax/addon/scalaz/validate.scala
@@ -14,10 +14,18 @@ trait validate {
 
 final class ValidateEffectScalazOps[R, A](private val e: Eff[R, A]) extends AnyVal {
 
-  def runValidationNel[U, E](r: Eff[R, A])(implicit m: Member.Aux[Validate[E, *], R, U]): Eff[U, ValidationNel[E, A]] =
+  @deprecated(message = "will be removed", since = "7.0.1")
+  private[scalaz] def runValidationNel[U, E](r: Eff[R, A])(implicit m: Member.Aux[Validate[E, *], R, U]): Eff[U, ValidationNel[E, A]] =
     addon.scalaz.validate.runValidationNel(e)
 
-  def runNelDisjunction[U, E](r: Eff[R, A])(implicit m: Member.Aux[Validate[E, *], R, U]): Eff[U, NonEmptyList[E] \/ A] =
+  def runValidationNel[U, E](implicit m: Member.Aux[Validate[E, *], R, U]): Eff[U, ValidationNel[E, A]] =
+    addon.scalaz.validate.runValidationNel(e)
+
+  @deprecated(message = "will be removed", since = "7.0.1")
+  private[scalaz] def runNelDisjunction[U, E](r: Eff[R, A])(implicit m: Member.Aux[Validate[E, *], R, U]): Eff[U, NonEmptyList[E] \/ A] =
+    addon.scalaz.validate.runNelDisjunction(e)
+
+  def runNelDisjunction[U, E](implicit m: Member.Aux[Validate[E, *], R, U]): Eff[U, NonEmptyList[E] \/ A] =
     addon.scalaz.validate.runNelDisjunction(e)
 
   def runMapDisjunction[U, E, L: Semigroup](map: E => L)(implicit m: Member.Aux[Validate[E, *], R, U]): Eff[U, L \/ A] =
