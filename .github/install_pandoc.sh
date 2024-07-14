76ab4aaa9b8bbd5180169a4b8ec00ea6ca9d4c4a
kenji yoshida <6b656e6a69@gmail.com>
2024-07-14 18:51:24 +0900

    Update pandoc

--- .github/install_pandoc.sh
+++ .github/install_pandoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-PANDOC_FILE_NAME="pandoc-3.1.13-1-amd64.deb"
-wget "https://github.com/jgm/pandoc/releases/download/3.1.13/${PANDOC_FILE_NAME}"
+PANDOC_FILE_NAME="pandoc-3.2.1-1-amd64.deb"
+wget "https://github.com/jgm/pandoc/releases/download/3.2.1/${PANDOC_FILE_NAME}"
 sudo dpkg -i "${PANDOC_FILE_NAME}"
 pandoc --version
