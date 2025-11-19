376776f86729b65a1581ef6b3536082fcfe8ef35
kenji yoshida <6b656e6a69@gmail.com>
2025-11-19 09:19:37 +0900

    Update Pandoc version to 3.8.2.1

--- .github/install_pandoc.sh
+++ .github/install_pandoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-PANDOC_VERSION="3.8.2"
+PANDOC_VERSION="3.8.2.1"
 PANDOC_FILE_NAME="pandoc-${PANDOC_VERSION}-1-amd64.deb"
 wget "https://github.com/jgm/pandoc/releases/download/${PANDOC_VERSION}/${PANDOC_FILE_NAME}"
 sudo dpkg -i "${PANDOC_FILE_NAME}"
